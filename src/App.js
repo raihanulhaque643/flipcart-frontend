5728c788e6d1d47de9618c8fba9ee29bdd779909
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-26 19:11:03 +0600

    signin user gets dynamic header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './containers/HomePage';
 import ProductListPage from './containers/ProductListPage';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { isUerLoggedIn } from './actions';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+
+
+  useEffect(() => {
+    if(!auth.authenticate){
+      dispatch(isUerLoggedIn());
+    }
+  }, [auth.autenticate]);
+
   return (
     <div className="App">
       <Router>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import flipkartLogo from "../../images/logo/flipcart.png";
 import goldenStar from "../../images/logo/golden-star.png";
@@ -9,7 +9,7 @@ import {
   MaterialButton,
   DropdownMenu,
 } from "../MaterialUI";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../actions/auth.actions";
 
 /**
@@ -21,11 +21,64 @@ const Header = (props) => {
   const [loginModal, setLoginModal] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const userLogin = () => {
-    dispatch(login({ email, password }))
-  }
+    dispatch(login({ email, password }));
+  };
+
+  useEffect(() => {
+    if (auth.authenticate) {
+    }
+  }, [auth.authenticate]);
+
+  const renderLoggedInMenu = () => {
+    return (
+      <DropdownMenu
+        menu={<a className="fullName">{auth.user.fullName}</a>}
+        menus={[
+          { label: "My Profile", href: "", icon: null },
+          { label: "SuperCoin Zone", href: "", icon: null },
+          { label: "Flipcart Plus Zone", href: "", icon: null },
+          { label: "Orders", href: "", icon: null },
+          { label: "Wishlist", href: "", icon: null },
+          { label: "My Chats", href: "", icon: null },
+          { label: "Coupons", href: "", icon: null },
+          { label: "Rewards", href: "", icon: null },
+          { label: "Notifications", href: "", icon: null },
+          { label: "Gift Cards", href: "", icon: null },
+          { label: "Logout", href: "", icon: null },
+        ]}
+      />
+    );
+  };
+
+  const renderNonLoggedInMenu = () => {
+    return (
+      <DropdownMenu
+        menu={
+          <a className="loginButton" onClick={() => setLoginModal(true)}>
+            Login
+          </a>
+        }
+        menus={[
+          { label: "My Profile", href: "", icon: null },
+          { label: "Flipkart Plus Zone", href: "", icon: null },
+          { label: "Orders", href: "", icon: null },
+          { label: "Wishlist", href: "", icon: null },
+          { label: "Rewards", href: "", icon: null },
+          { label: "Gift Cards", href: "", icon: null },
+        ]}
+        firstMenu={
+          <div className="firstmenu">
+            <span>New Customer?</span>
+            <a style={{ color: "#2874f0" }}>Sign Up</a>
+          </div>
+        }
+      />
+    );
+  };
 
   return (
     <div className="header">
@@ -105,27 +158,7 @@ const Header = (props) => {
           </div>
         </div>
         <div className="rightMenu">
-          <DropdownMenu
-            menu={
-              <a className="loginButton" onClick={() => setLoginModal(true)}>
-                Login
-              </a>
-            }
-            menus={[
-              { label: "My Profile", href: "", icon: null },
-              { label: "Flipkart Plus Zone", href: "", icon: null },
-              { label: "Orders", href: "", icon: null },
-              { label: "Wishlist", href: "", icon: null },
-              { label: "Rewards", href: "", icon: null },
-              { label: "Gift Cards", href: "", icon: null },
-            ]}
-            firstMenu={
-              <div className="firstmenu">
-                <span>New Customer?</span>
-                <a style={{ color: "#2874f0" }}>Sign Up</a>
-              </div>
-            }
-          />
+          {!auth.authenticate ? renderNonLoggedInMenu() : renderLoggedInMenu()}
           <DropdownMenu
             menu={
               <a className="more">
